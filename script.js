49ffb546b843352502a7492d21789557aaa770bf
James Totah <jtotah@usfca.edu>
2020-11-06 16:50:23 -0800

    UPDATE: Complete functionality

--- script.js
+++ script.js
@@ -41,13 +41,15 @@ $(document).ready(function () {
     }
 
     let fillBanner = function (query) {
-        $("#city-text").text(query);
 
         let searchQ = "https://api.openweathermap.org/data/2.5/forecast?q=" + updateCity(query) + "&units=imperial&appid=b8cf73639b0d81c1905ba1ac1cb6f289";
         fetch(searchQ)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                let date = moment(data.list[4].dt_txt).subtract(1, 'days').format("L");
+
+                $("#city-text").text(query + " " + date);
                 $("#temp").text("Temperature: " + data.list[4].main.temp + " °F");
                 $("#humidity").text("Humidity: " + data.list[4].main.humidity + "%");
                 $("#wind").text("Wind Speed: " + data.list[4].wind.speed + "mph");
@@ -70,6 +72,39 @@ $(document).ready(function () {
                         }
                         console.log(data);
                     })
+                $("#card-row").empty();
+                for (let i = 6; i < data.list.length; i += 8) {
+                    cardCreator(data.list[i]);
+                }
             })
     }
+
+    let cardCreator = function (data) {
+        let iconcode = data.weather[0].icon;
+
+        let card = $("<div>");
+        let cardBody = $("<div>");
+        let cardHeader = $("<h5>");
+        let cardIcon = $("<img>");
+        let cardTemp = $("<p>");
+        let cardHum = $("<p>");
+
+        card.attr("style", "width: 10rem; margin-right: 4px;");
+        card.addClass("card");
+        cardBody.addClass("card-body card-days");
+        cardHeader.addClass("card-title");
+        cardIcon.attr("src", "http://openweathermap.org/img/w/" + iconcode + ".png");
+
+        cardHeader.text(moment(data.dt_txt).format("L"));
+        cardTemp.text("Temp: " + data.main.temp + " °F");
+        cardHum.text("Humidity:" + data.main.humidity + "%");
+
+        cardBody.append(cardHeader);
+        cardBody.append(cardIcon);
+        cardBody.append(cardTemp);
+        cardBody.append(cardHum);
+        card.append(cardBody);
+        $("#card-row").append(card);
+    }
+
 })
